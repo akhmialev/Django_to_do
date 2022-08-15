0bf374f23faa356c9c6f494e29694c183bb85f2f
20390680 <20390680>
2022-08-15 14:34:18 +0300

    finish project

--- todowo_project/todo/views.py
+++ todowo_project/todo/views.py
@@ -48,7 +48,7 @@ def loginuser(request):
     if request.method == 'GET':
         return render(request, 'loginuser.html', {'form': AuthenticationForm})
     else:
-        user = authenticate(request, username=request.POST['username'], password=request.POST['password'] )
+        user = authenticate(request, username=request.POST['username'], password=request.POST['password'])
         if user is None:
             return render(request, 'loginuser.html', {'form': AuthenticationForm, 'error': 'Username and password did not match'})
         else:
--- todowo_project/todowo_project/urls.py
+++ todowo_project/todowo_project/urls.py
@@ -22,7 +22,7 @@ urlpatterns = [
     #auth
     path('signup/', signupuser, name='signupuser'),
     path('logout/', logoutuser, name='logoutuser'),
-    path('login/', loginuser, name='logintuser'),
+    path('login/', loginuser, name='loginuser'),
     #Todos
     path('', home, name='home'),
     path('create/', createdtodo, name='createdtodo'),
